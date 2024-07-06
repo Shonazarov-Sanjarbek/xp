c7e6b04986582995235914bc9a812bc3e7ba663f
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-07-06 21:50:25 +0500

    homework-15

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const sidebar = document.querySelector(".navbar__sidebar")
+
+function sidebarOpen() {
+    sidebar.classList.toggle("navbar__show")
+    // let bool = sidebar.classList.contains("navbar__show")
+    // if (sidebar.classList.contains("navbar__show")) {
+    //     sidebar.classList.remove("navbar__show")
+    // }else{
+    //     sidebar.classList.add("navbar__show")
+    // }
+}
+
